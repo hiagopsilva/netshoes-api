fd0f6583c87f490bd3fb2654fd5d1578034f7a22
Hiago Prates <hiagohps360@gmail.com>
2024-07-02 19:01:59 -0300

    feat: format values list

--- src/http/controllers/product/list.ts
+++ src/http/controllers/product/list.ts
@@ -23,8 +23,18 @@ export async function listProduct(
       isFavorite,
     })
 
-    return reply.status(200).send(product)
+    const listProducts = product.map((product) => {
+      return {
+        _id: product._id,
+        name: product.name,
+        image: product.product[0].image,
+        isFavorite: product.isFavorite,
+      }
+    })
+
+    return reply.status(200).send(listProducts)
   } catch (err) {
+    console.log({ err })
     reply.status(500).send({ message: 'Internal server error', error: err })
   }
 }
